fadc83965af6bf32807a14818eedd8bb09442df9
Dabiwel22 <potthoffdaniel@gmail.com>
2015-10-30 08:58:53 -0500

    changed the game

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,7 +12,10 @@ namespace WindowsFormsApplication1
 {
     public partial class frmSkaro : Form
     {
-        bool Skaro = false; 
+        bool Skaro = false;
+        bool Earth = false;
+        bool Galifrey = false;
+        bool Davros = false; 
         public frmSkaro()
         {
             InitializeComponent();
@@ -27,17 +30,23 @@ namespace WindowsFormsApplication1
 
         private void btnDavros_Click(object sender, EventArgs e)
         {
-
+            Davros = true;
+            btnDavros.Visible = false;
+            MessageBox.Show("How did you find Davros you aren't on Skaro yet.");
         }
 
         private void btnEarth_Click(object sender, EventArgs e)
         {
-
+            Galifrey = true;
+            btnGalifrey.Visible = false;
+            MessageBox.Show("Find the T.A.R.D.I.S");
         }
 
         private void btnGalifrey_Click(object sender, EventArgs e)
         {
-
+            Earth = true;
+            btnEarth.Visible = false;
+            MessageBox.Show("Find Clara");
         }
     }
 }
